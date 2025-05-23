1be2c0a54cb9b1a4a9f685fa93818dad550870f5
tassad <tassadaritze@gmail.com>
2025-05-23 14:19:52 +0300

    solve tasks for step 1.1

--- 1_concepts/1_1_default_clone_copy/src/main.rs
+++ 1_concepts/1_1_default_clone_copy/src/main.rs
@@ -1,3 +1,24 @@
+#[derive(Copy, Clone, Default)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Clone)]
+struct Polyline {
+    // I'm assuming the "set" in "represents a non-empty set" is not meant literally
+    points: Vec<Point>,
+}
+
 fn main() {
-    println!("Implement me!");
+    let a = Point { x: -1, y: 1 };
+    let b = a;
+    let c = a;
+
+    let polyline = Polyline {
+        points: vec![a, b, c],
+    };
+    // let polyline_b = polyline;
+    let polyline_b = polyline.clone();
+    let polyline_c = polyline;
 }
