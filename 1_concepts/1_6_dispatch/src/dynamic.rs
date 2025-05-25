01fd0c2e79e17137c9064a5dd7d20d977aa42025
tassad <tassadaritze@gmail.com>
2025-05-25 12:07:31 +0300

    solve task 1 for step 1.6

---
+++ 1_concepts/1_6_dispatch/src/dynamic.rs
@@ -0,0 +1,48 @@
+use std::borrow::Cow;
+use std::collections::HashMap;
+
+use crate::{Storage, User};
+
+struct UserRepository {
+    storage: Box<dyn Storage<u64, User>>,
+}
+
+impl UserRepository {
+    fn new(storage: impl Storage<u64, User> + 'static) -> Self {
+        Self {
+            storage: Box::new(storage),
+        }
+    }
+}
+
+impl Storage<u64, User> for UserRepository {
+    fn set(&mut self, key: u64, val: User) {
+        self.storage.set(key, val);
+    }
+
+    fn get(&self, key: &u64) -> Option<&User> {
+        self.storage.get(key)
+    }
+
+    fn remove(&mut self, key: &u64) -> Option<User> {
+        self.storage.remove(key)
+    }
+}
+
+pub(super) fn run() {
+    let mut repository = UserRepository::new(HashMap::new());
+    let user = User {
+        id: 0,
+        email: Default::default(),
+        activated: false,
+    };
+
+    repository.set(0, user);
+    assert_eq!(repository.get(&0).map(|u| u.id), Some(0));
+    assert_eq!(repository.get(&0).map(|u| &u.email), Some(&Cow::default()));
+    assert_eq!(repository.get(&0).map(|u| u.activated), Some(false));
+
+    assert_eq!(repository.remove(&0).map(|u| u.id), Some(0));
+    assert!(repository.remove(&0).is_none());
+    assert!(repository.get(&0).is_none());
+}
--- 1_concepts/1_6_dispatch/src/main.rs
+++ 1_concepts/1_6_dispatch/src/main.rs
@@ -1,3 +1,38 @@
+mod dynamic;
+
+use std::borrow::Cow;
+use std::collections::HashMap;
+use std::hash::Hash;
+
+trait Storage<K, V> {
+    fn set(&mut self, key: K, val: V);
+    fn get(&self, key: &K) -> Option<&V>;
+    fn remove(&mut self, key: &K) -> Option<V>;
+}
+
+struct User {
+    id: u64,
+    email: Cow<'static, str>,
+    activated: bool,
+}
+
+impl<K, V> Storage<K, V> for HashMap<K, V>
+where
+    K: Eq + Hash,
+{
+    fn set(&mut self, key: K, val: V) {
+        self.insert(key, val);
+    }
+
+    fn get(&self, key: &K) -> Option<&V> {
+        self.get(key)
+    }
+
+    fn remove(&mut self, key: &K) -> Option<V> {
+        self.remove(key)
+    }
+}
+
 fn main() {
-    println!("Implement me!");
+    dynamic::run();
 }
